81c32c182613325e872696e1a6129a1880ddb594
javier12s <151806272+javier12s@users.noreply.github.com>
2024-09-07 16:41:20 -0600

    Add files via upload

---
+++ estructura ssab.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+diccionario_listas={
+    "nombre":["circo","imelda","jose"],
+    "edad":[22,32,15],
+    "Area":["ti","rh","produccion"]
+    }
+df=pd.DataFrame(diccionarios_listas)
+
+df
